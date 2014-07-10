c65faed5d5247f4b3c2456acc3870926c02d77af
Takahito Nara <takahitonara@gmail.com>
2014-07-11 01:32:57 +0900

    Add the current Cask file and init.el, setting file of emacs.

---
+++ .emacs.d/init.el
@@ -0,0 +1,250 @@
+;; ====================================
+;; Starup Page
+;; ====================================
+;; スタートアップページを表示しない
+(setq inhibit-startup-message t)
+
+
+;; ====================================
+;; Key bindings
+;; ====================================
+;; Set C-h delete key
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; ====================================
+;; Setting GUI
+;; ====================================
+(custom-set-variables
+  '(auto-save-default nil)
+  '(global-linum-mode t))
+
+;; ====================================
+;; Backup file setting
+;; ====================================
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+;;(setq auto-save-default nil)
+
+;; ====================================
+;; howm
+;; ====================================
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/howm/")
+(setq howm-menu-lang 'ja)
+(require 'howm)
+
+;; リンクを TAB で辿る
+(eval-after-load "howm-mode"
+  '(progn
+     (define-key howm-mode-map [tab] 'action-lock-goto-next-link)
+     (define-key howm-mode-map [(meta tab)] 'action-lock-goto-previous-link)))
+;; 「最近のメモ」一覧時にタイトル表示
+(setq howm-list-recent-title t)
+;; 全メモ一覧時にタイトル表示
+(setq howm-list-all-title t)
+;; メニューを 2 時間キャッシュ
+(setq howm-menu-expiry-hours 2)
+
+;; RET でファイルを開く際, 一覧バッファを消す
+;; C-u RET なら残る
+(setq howm-view-summary-persistent nil)
+
+;; メニューの予定表の表示範囲
+;; 10 日前から
+(setq howm-menu-schedule-days-before 10)
+;; 3 日後まで
+(setq howm-menu-schedule-days 3)
+
+;; howm のファイル名
+;; 以下のスタイルのうちどれかを選んでください
+;; で，不要な行は削除してください
+;; 1 メモ 1 ファイル (デフォルト)
+(setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.howm")
+;; 1 日 1 ファイルであれば
+;; (setq howm-file-name-format "%Y/%m/%Y-%m-%d.howm")
+
+(setq howm-view-grep-parse-line
+      "^\\(\\([a-zA-Z]:/\\)?[^:]*\\.howm\\):\\([0-9]*\\):\\(.*\\)$")
+;; 検索しないファイルの正規表現
+(setq
+ howm-excluded-file-regexp
+ "/\\.#\\|[~#]$\\|\\.bak$\\|/CVS/\\|\\.doc$\\|\\.pdf$\\|\\.ppt$\\|\\.xls$")
+
+;; いちいち消すのも面倒なので
+;; 内容が 0 ならファイルごと削除する
+(if (not (memq 'delete-file-if-no-contents after-save-hook))
+    (setq after-save-hook
+          (cons 'delete-file-if-no-contents after-save-hook)))
+(defun delete-file-if-no-contents ()
+  (when (and
+         (buffer-file-name (current-buffer))
+         (string-match "\\.howm" (buffer-file-name (current-buffer)))
+         (= (point-min) (point-max)))
+    (delete-file
+     (buffer-file-name (current-buffer)))))
+
+;; C-cC-c で保存してバッファをキルする
+(defun my-save-and-kill-buffer ()
+  (interactive)
+  (when (and
+         (buffer-file-name)
+         (string-match "\\.howm"
+                       (buffer-file-name)))
+    (save-buffer)
+    (kill-buffer nil)))
+(eval-after-load "howm"
+  '(progn
+     (define-key howm-mode-map
+       "\C-c\C-c" 'my-save-and-kill-buffer)))
+
+;; ====================================
+;; cask
+;; ====================================
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;;複数ウィンドウを開かないようにする
+;;(setq ns-pop-up-frames nil)
+
+(if window-system (server-start))
+
+;; ====================================
+;; helm
+;; ====================================
+(when (require 'helm-config nil t)
+  (helm-mode 1)
+
+  (define-key global-map (kbd "M-x")     'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+  (define-key global-map (kbd "C-c i")   'helm-imenu)
+  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
+
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+  ;; Disable helm in some functions
+  (add-to-list 'helm-completing-read-handlers-alist '(find-alternate-file . nil))
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+           (input-pattern (file-name-nondirectory pattern))
+           (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+            (concat dirname
+                    (if (string-match "^\\^" input-pattern)
+                        ;; '^' is a pattern for basename
+                        ;; and not required because the directory name is prepended
+                        (substring input-pattern 1)
+                      (concat ".*" input-pattern)))))))
+
+
+
+;; ====================================
+;; powerline
+;; ====================================
+(require 'powerline)
+(powerline-default-theme)
+
+
+;; ====================================
+;; howm
+;; ====================================
+;; リンクを TAB で辿る
+(eval-after-load "howm-mode"
+  '(progn
+     (define-key howm-mode-map [tab] 'action-lock-goto-next-link)
+     (define-key howm-mode-map [(meta tab)] 'action-lock-goto-previous-link)))
+
+;; メニューを 2 時間キャッシュ
+(setq howm-menu-expiry-hours 2)
+
+;; RET でファイルを開く際, 一覧バッファを消す
+;; C-u RET なら残る
+(setq howm-view-summary-persistent nil)
+
+;; メニューの予定表の表示範囲
+;; 10 日前から
+(setq howm-menu-schedule-days-before 10)
+;; 3 日後まで
+(setq howm-menu-schedule-days 3)
+
+;; いちいち消すのも面倒なので
+;; 内容が 0 ならファイルごと削除する
+(if (not (memq 'delete-file-if-no-contents after-save-hook))
+    (setq after-save-hook
+          (cons 'delete-file-if-no-contents after-save-hook)))
+(defun delete-file-if-no-contents ()
+  (when (and
+         (buffer-file-name (current-buffer))
+         (string-match "\\.howm" (buffer-file-name (current-buffer)))
+         (= (point-min) (point-max)))
+    (delete-file
+     (buffer-file-name (current-buffer)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; disable menu bar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(tool-bar-mode 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; disable tool bar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(menu-bar-mode 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; git-gutter-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-git-gutter-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; zenburn
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load-theme 'zenburn t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; high light current line
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background  "#98FB98"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+(global-hl-line-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; open-junk-file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'open-junk-file)
+(setq open-junk-file-format "~/.junk/%Y-%m-%d-%H%M%S.")
+(global-set-key (kbd "C-x j") 'open-junk-file)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; auto-highlight-symbol-config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'auto-highlight-symbol)
+(global-auto-highlight-symbol-mode t)
